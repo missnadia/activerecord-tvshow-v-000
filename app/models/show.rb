758fd468d1e2d5812058bed2b7e42f5928b8aa32
Nadia Lee <miss.nadia.lee@gmail.com>
2018-03-27 20:47:17 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,27 @@
 class Show < ActiveRecord::Base
+  def highest_rating
+    self.maximum(:rating)
+  end
+
+  def most_popular_show
+
+  end
+
+  def lowest_rating
+    self.minimum(:rating)
+  end
+
+  def least_popular_show
+  end
+
+  def ratings_sum
+    self.sum(:rating)
+  end
+
+  def popular_shows
+  end
+
+  def shows_by_alphabetical_order
+    self.order(:name)
+  end
 end
